555e5569070d231bc896432e208de56ae08a2829
andrejs233 <84469424+andrejs233@users.noreply.github.com>
2022-04-12 01:02:11 -0300

    Update 057.py

--- 057.py
+++ 057.py
@@ -3,4 +3,3 @@ sexo = str(input('Informe seu sexo[M/F]:')).strip().upper()[0]
 while sexo not in 'MmFf':
     sexo = str(input('Dados invalidos. Informe seu sexo:'))
 print('Sexo {} registrado com sucesso'.format(sexo))
-#Feito somente com ajuda.(fracassado)
